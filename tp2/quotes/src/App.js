a3d0ce9264c4fe390c5ec2bb65dbe0068eaf8b66
Nathan COQUART <coquartnathan@gmail.com>
2024-01-29 12:39:43 +0100

    niah

--- tp2/quotes/src/App.js
+++ tp2/quotes/src/App.js
@@ -5,12 +5,13 @@ import quotes from './Data/data';
 function Quote(props) {
   return (
     <>
-      <div>
+      <div class="textecita">
         Texte : {props.text}
       </div>
-      <div>
+      <div  class="authorcita">
         Auteur : {props.author}
       </div>
+      <br/>
     </>
   );
 }
