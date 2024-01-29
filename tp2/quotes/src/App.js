678e68ac097774a60ae493291e24a427cd285858
Novais <abigail62810@gmail.com>
2024-01-29 12:12:56 +0100

    erreur

--- tp2/quotes/src/App.js
+++ tp2/quotes/src/App.js
@@ -1,20 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
 
+function Quote(props) {
+  const { text, author } = props;
+
+  return (
+    <>
+      <div>
+        Texte : {text}
+      </div>
+      <div>
+        Auteur : {author}
+      </div>
+    </>
+  );
+}
+
+  const citation1 = {
+    text: "Première citation",
+    author: "Auteur 1"
+  };
+
+  const citation2 = {
+    text: "Deuxième citation",
+    author: "Auteur 2"
+  };
+
 function App() {
   return (
-    <div className="App">
-      <h1>
-      Mes citations !
-      </h1>
-      <div>
-      Texte: "Elementary, my dear Watson"
-      </div>
-      <div>
-      Auteur: "Sherlock Holmes"
-      </div>
+    <div>
+      <Quote text={citation1.text} author={citation1.author} />
+      <Quote text={citation2.text} author={citation2.author} />
     </div>
   );
 }
 
 export default App;
+
