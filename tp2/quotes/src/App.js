ff677c53bd2064d8d1d55daad7f9b329eeea3aa2
Nathan COQUART <coquartnathan@gmail.com>
2024-01-29 12:27:37 +0100

    vdv

--- tp2/quotes/src/App.js
+++ tp2/quotes/src/App.js
@@ -16,9 +16,9 @@ function Quote(props) {
 
 function App() {
   const quotes = [
-    { id: 14, text: "Elementary, my dear Watson", author: "Sherlock Holmes" },
-    { id: 56, text: "I think therefore I am", author: "Rene Descartes" },
-    { id: 3, text: "Big Brother is watching you", author: "George Orwell" },
+    { id: 14, text: "Dans le doute pied au plancher", auteur: "Grand tourismo" },
+    { id: 56, text: "Qelque chose en l'aire fini par tombe par terre FORT", auteur: "Yooouuutoonne"  },
+    { id: 3, text: "Ne met pas tes doigts dans la porte tu risque de te faire mal", auteur: "Un lapin rose" },
     { id: 41, text: "You talkin' to me?", author: "Travis Bickle" }
   ];
 
