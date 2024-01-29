e28e8eed45e67f6fa6a4038784baf9601f84e4df
Abigail Novais <146720749+AbiNovais@users.noreply.github.com>
2024-01-29 11:57:28 +0100

    Add files via upload

---
+++ tp2/quotes/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+      Mes citations !
+      </h1>
+      <div>
+      Texte: "Elementary, my dear Watson"
+      </div>
+      <div>
+      Auteur: "Sherlock Holmes"
+      </div>
+    </div>
+  );
+}
+
+export default App;
